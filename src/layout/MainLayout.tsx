79c1369a6c3d81ed8fb45a351ba802d7a0d12feb
Kurt <kurtjonathangozano@gmail.com>
2025-08-26 13:24:21 +0800

    footer error fix

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-import Footer from "../components/Footer";
+import Footer from "../components/footer";
 
 export default function MainLayout() {
   return (
@@ -7,7 +7,6 @@ export default function MainLayout() {
       <main>
         <Outlet />
       </main>
-      {/*  */}
       <Footer />
     </div>
   );
